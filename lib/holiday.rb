34e70c7742f737a01701c0fa27f49aaae77fdcd2
Kevin C Malin <racmalin@yahoo.com>
2020-06-09 01:00:54 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,22 +1,4 @@
 
-  def all_holidays_with_bbq(holiday_hash)
-    i=0
-    new_holiday = []
-      holiday_hash.each do |season, data|
-        data.each do |holiday, supplies|
-          if supplies.include?("BBQ")
-            new_holiday << holiday
-          end
-        end
-      end
-      new_holiday
-  end
-
-
-
-# all_holidays_with_bbq(holiday_hash)
-
-
-
+  
 
 
