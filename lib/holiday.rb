114539f4a02429dba06cf1b9fd9cd3379253c999
Kevin C Malin <racmalin@yahoo.com>
2020-06-09 00:59:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,20 +1,4 @@
-# # require 'pry'
 
-# holiday_hash = {
-#   :winter => {
-#     :christmas => ["Lights", "Wreath"],
-#     :new_years => ["Party Hats"]
-#   },
-#   :summer => {
-#     :fourth_of_july => ["Fireworks", "BBQ"]
-#   },
-#   :fall => {
-#     :thanksgiving => ["Turkey"]
-#   },
-#   :spring => {
-#     :memorial_day => ["BBQ"]
-#   }
-# }
   def all_holidays_with_bbq(holiday_hash)
     i=0
     new_holiday = []
